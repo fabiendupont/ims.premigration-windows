8cf8c5d2d7ac81f52d5cca16530441abfe41b883
Fabien Dupont <eveni@WhiteShark.home.evenit.info>
2020-06-11 17:22:47 +0200

    Add doc and comments in the PowerShell script

--- files/pre-migrate.ps1
+++ files/pre-migrate.ps1
@@ -1,5 +1,46 @@
-# The constants we use for the script
-# Various paths
+<#
+.SYNOPSIS
+  This script extracts the network adapters IP configuration when
+  configuration is static, as well as the drives letters. This configuration
+  is written is a script that will be run during next boot. It also installs
+  RHV-APT if it is absent.
+
+.DESCRIPTION
+  When migrating a Windows virtual machine from VMware to KVM, the drivers
+  are modified by virt-v2v. The consequence is that the new network adapters
+  are created with the same MAC addresses as the original network adapters.
+  With Windows, the new network adapters IP configuration is not bound to the
+  the MAC addresses, so the new network adapters are not configured, so the
+  virtual machine is unreachable.
+
+  Another potential hickup is that drives may be ordered differently after the
+  migration. This may lead to having wrong drive letters and programs not being
+  able to find their data. 
+
+  We also have noticed that sometimes RHV-APT is not installed after the
+  migration, even though virt-v2v first boot scripts have run successfully.
+  The consequence is that, even though the VirtIO Win ISO image is attached to
+  the virtual machine, the additional drivers and the RHV Guest Agent will not
+  be installed.
+
+  This script collects the configuration of the Windows virtual machine and
+  adds commands to a new script, in order to:
+
+    1. Disable the original network adapters to avoid conflict after migration.
+    2. Configure the new network adapters based on MAC address.
+    3. Configure the drives letters based on WMI object id.
+    4. Install and start RHV-APT if it is absent.
+
+  In order to run the generated script, we also create a batch file (.bat)
+  under C:\Program Files\Guestfs\Firstboot\scripts, so that virt-v2v first boot
+  script runs it. This avoids creating another scheduled task.
+
+.NOTES
+  Version:        1.0
+  Author:         Fabien Dupont <fdupont@redhat.com>
+  Purpose/Change: Extract system configuration during premigration
+#>
+
 $scriptDir = "C:\Program Files\Guestfs\Firstboot\scripts"
 $restoreScriptFile = $scriptDir + "\9999-restore_config.ps1"
 $firstbootScriptFile = $scriptDir + "\9999-restore_config.bat"
@@ -9,15 +50,19 @@ if (!(Get-Item $scriptDir -ErrorAction SilentlyContinue)) {
     New-Item -Type directory -Path $scriptDir
 }
 
-# Initialize the script
+# Initialize the generated script
 Write-Output ("# Migration - Reconfigure network adapters and disks") > $restoreScriptFile
 Write-Output ("") >> $restoreScriptFile
 
+# Initialize the log file created by the generated script
 Write-Output ("`$logFile = `$env:SystemDrive + '\Program Files\Guestfs\Firstboot\scripts-done\9999-restore_config.txt'") >> $restoreScriptFile
 Write-Output ("Write-Output ('Starting restore_config.ps1 script') > `$logFile") >> $restoreScriptFile
 Write-Output ("Write-Output ('') >> `$logFile") >> $restoreScriptFile
 Write-Output ("") >> $restoreScriptFile
 
+# Make the generated script exit if run on VMware.
+# If it runs on VMware, it means that the migration rolled back, so
+# we don't want to impact the configuration of the source virtual machine.
 Write-Output ("# Exit if the machine is still on VMware") >> $restoreScriptFile
 Write-Output ("`$system = Get-WmiObject Win32_ComputerSystem") >> $restoreScriptFile
 Write-Output ("if (`$system.Manufacturer -eq 'VMware, Inc.') {") >> $restoreScriptFile
@@ -26,6 +71,7 @@ Write-Output ("    Exit") >> $restoreScriptFile
 Write-Output ("}") >> $restoreScriptFile
 Write-Output ("") >> $restoreScriptFile
 
+# Generate the script section for each network adapter
 Get-NetAdapter | ForEach-Object {
     Write-Output ("Write-Output ('Restoring network adapater for MAC address " + $_.MacAddress + "') >> `$logFile") >> $restoreScriptFile
     Write-Output ("# Disable the non-Red Hat network adapter with the MAC address") >> $restoreScriptFile
@@ -64,8 +110,9 @@ Get-NetAdapter | ForEach-Object {
     Write-Output ("") >> $restoreScriptFile
 }
 
-Write-Output ("# Re-enable all offline disks") >> $restoreScriptFile
-Write-Output ("Write-Output ('Re-enabling all offline disks') >> `$logFile") >> $restoreScriptFile
+# Generate the script section to re-enable all offline drives
+Write-Output ("# Re-enable all offline drives") >> $restoreScriptFile
+Write-Output ("Write-Output ('Re-enabling all offline drives') >> `$logFile") >> $restoreScriptFile
 Write-Output ("Get-Disk | Where { `$_.OperationalStatus -like 'Offline' } | % {") >> $restoreScriptFile
 Write-Output ("     Write-Output ('  - ' + `$_.Number + ': ' + `$_.FriendlyName + '(' + [math]::Round(`$_.Size/1GB,2) + 'GB)') >> `$logFile") >> $restoreScriptFile
 Write-Output ("    `$_ | Set-Disk -IsOffline `$false") >> $restoreScriptFile
@@ -74,7 +121,9 @@ Write-Output ("}") >> $restoreScriptFile
 Write-Output ("Write-Output ('') >> `$logFile") >> $restoreScriptFile
 Write-Output ("") >> $restoreScriptFile
 
-Write-Output ("# Remove the partition access path on all partitions but SystemDrive") >> $restoreScriptFile
+# Generate the script section to remove the access path on all partitions
+# but SystemDrive
+Write-Output ("# Remove the access path on all partitions but SystemDrive") >> $restoreScriptFile
 Write-Output ("`$a = (Get-Item env:SystemDrive).Value.substring(0,1)") >> $restoreScriptFile
 Write-Output ("Write-Output('Remove the partition access path on all partitions but SystemDrive (" + $a +")') >> `$logFile") >> $restoreScriptFile
 Write-Output ("Get-Partition | Where { `$_.DriveLetter -notlike `$a -and `$_.DriveLetter.length -gt 0 } | % {") >> $restoreScriptFile
@@ -89,7 +138,7 @@ Write-Output ("}") >> $restoreScriptFile
 Write-Output ("Write-Output ('') >> `$logFile") >> $restoreScriptFile
 Write-Output ("") >> $restoreScriptFile
 
-# Find the current CD-ROM drive
+# Gnerate the script section to restore the drive letters
 Write-Output ("# Restore drive letters") >> $restoreScriptFile
 Write-Output ("Write-Output ('Restore drive letters') >> `$logFile") >> $restoreScriptFile
 Get-Volume | ForEach-Object {
@@ -110,6 +159,7 @@ Get-Volume | ForEach-Object {
 Write-Output ("Write-Output ('') >> `$logFile") >> $restoreScriptFile
 Write-Output ("") >> $restoreScriptFile
 
+# Generate the script section to install and start RHV-APT service
 Write-Output ("# Install RHV APT service if absent") >> $restoreScriptFile
 Write-Output ("if ((Get-Service rhev-apt -ErrorAction SilentlyContinue)) {") >> $restoreScriptFile
 Write-Output ("    Write-Output('Service rhev-apt is already installed. Skipping.') >> `$logFile") >> $restoreScriptFile
@@ -129,7 +179,7 @@ Write-Output ("") >> $restoreScriptFile
 
 Write-Output ("# Start rhev-apt service if present.') >> `$logFile") >> $restoreScriptFile
 Write-Output ("if (Get-Service rhev-apt -ErrorAction SilentlyContinue) {") >> $restoreScriptFile
-Write-Output ("    Write-Output ('Service rhev-apt is present. Starting it.') >> `$logFile") >> $scripFile
+Write-Output ("    Write-Output ('Service rhev-apt is present. Starting it.') >> `$logFile") >> $restoreScriptFile
 Write-Output ("    Start-Service -Name rhev-apt") >> $restoreScriptFile
 Write-Output ("}") >> $restoreScriptFile
 Write-Output ("else {") >> $restoreScriptFile
@@ -138,7 +188,8 @@ Write-Output ("}") >> $restoreScriptFile
 Write-Output ("Write-Output ('') >> `$logFile") >> $restoreScriptFile
 Write-Output ("") >> $restoreScriptFile
 
-# Using Out-File to set encoding and avoid UTF-8 BOM
+# Generate the batch script that will be run by virt-v2v first boot script
+# Using Out-File instead of Write-Output to set encoding and avoid UTF-8 BOM
 '@echo off' | Out-File -FilePath $firstbootScriptFile -Encoding ascii 
 '' | Out-File -FilePath $firstbootScriptFile -Encoding ascii -Append
 'echo Restore configuration for network adapters and disks' | Out-File -FilePath $firstbootScriptFile -Encoding ascii -Append
